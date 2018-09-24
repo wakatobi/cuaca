e9fa59e91da30fa53699bf13d947ee78dfcb90e3
Nicholas Steven <nicholassteven998@gmail.com>
2018-09-24 01:24:57 -0700

    Add alternate function output
    Displays message instead of card

--- js/weather.js
+++ js/weather.js
@@ -1,3 +1,49 @@
+/* function insertWeatherData (x) {
+    // creates article element to display weather data
+    var elementToAdd = document.createElement("article");
+    elementToAdd.className = "message";
+    elementToAdd.classList.add("is-primary");
+    elementToAdd.classList.add("is-large")
+
+    // creates message element in article
+    var child = document.createElement("div");
+    child.className = "message-body";
+
+    // create texts and appends it to the message element created
+    var node = document.createTextNode(x);
+    child.appendChild(node);
+
+    // appends message to article
+    elementToAdd.appendChild(child);
+    elementToAdd.id = "weatherObject";
+
+    // creates a button
+    var celsius = document.createElement("button");
+    celsius.className = "button";
+    celsius.classList.add("is-primary");
+    celsius.classList.add("is-outlined");
+    celsius.textContent = "C";
+    celsius.id = "celsius";
+
+    var farenheit = document.createElement("button");
+    farenheit.className = "button";
+    farenheit.classList.add("is-primary");
+    farenheit.classList.add("is-outlined");
+    farenheit.textContent = "F";
+    farenheit.id = "fahrenheit";
+
+    // creates new row
+    var newDiv = document.createElement('div');
+    newDiv.appendChild(celsius);
+    newDiv.appendChild(farenheit);
+    
+    // appends article to container
+    document.getElementById("contain").appendChild(elementToAdd);
+    document.getElementById("contain").appendChild(newDiv);
+
+
+} */
+
 
 function convert (x, text){
     var final = x - 273.15;
